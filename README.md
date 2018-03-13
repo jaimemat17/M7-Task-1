164ef62fa9f3ff2276ea33a04d90d36dcee3768b
jaimettbb <jaimemat17@outlook.com>
2018-03-14 00:30:36 +0100

    Exercise Finished

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
 # M7-Task-1
+
+In this task we start with the next barchar:
+
+![Alt text](images/Screenshot_1.png?raw=true "Title")
+
+And we have to:
+
+    - Adding space between columns.
+    - Adding colors to each bar.
+    - Adding a legend.
+    - Showing the chart vertically.
+
+Let's do the previous tasks step by step.
+
+## Adding space between columns
+
+We need to change the next part of the code in the appendChartBars function:
+ 
+![Alt text](images/Screenshot_2.png?raw=true "Title")
+
+By the next one:
+
+![Alt text](images/Screenshot_3.png?raw=true "Title")
+
+It's just been subtracted 5 points to the current band width.
+
+## Adding colors to each bar
+
+We need to create a new variable barColor. We can put it before call the functions.
+
+![Alt text](images/Screenshot_4.png?raw=true "Title")
+
+We also need to add a new attribute to the newRects append of the appendChartBars function:
+
+![Alt text](images/Screenshot_5.png?raw=true "Title")
+
+## Adding a legend
+
+We should add the next function to the code
+
+![Alt text](images/Screenshot_6.png?raw=true "Title")
+
+and call to the function
+
+![Alt text](images/Screenshot_7.png?raw=true "Title")
+
+The attribute transform-transalte changes according to your chart size and your configuration. It depends on where you want to place the legend.
+
+## Showing the chart vertically
+
+We are going to rotate the whole chart. In order to do that we need to change: 
+
+    - The Canvas size
+        
+![Alt text](images/Screenshot_8.png?raw=true "Title")
+
+    - Rotate the axis
+
+![Alt text](images/Screenshot_9.png?raw=true "Title")
+
+    - Rotate the bars in the the newRects append of the appendChartBars function
+
+![Alt text](images/Screenshot_10.png?raw=true "Title")
+
+    - Change the axisBottom function by the axisTop function
+
+![Alt text](images/Screenshot_12.png?raw=true "Title")
+
+The next image shows the result.
+
+![Alt text](images/Screenshot_11.png?raw=true "Title")
--- main - copia.js
+++ main - copia.js
@@ -1,3 +1,4 @@
+
 // Let's start using ES6
 // And let's organize the code following clean code concepts
 // Later one we will complete a version using imports + webpack
@@ -10,7 +11,6 @@ let margin = null,
 
 let svg = null;
 let x, y = null; // scales
-var barColor = d3.scaleOrdinal(d3.schemeCategory20);
 
 setupCanvasSize();
 appendSvg("body");
@@ -19,13 +19,12 @@ setupYScale();
 appendXAxis();
 appendYAxis();
 appendChartBars();
-AppendLegend();
 
 // 1. let's start by selecting the SVG Node
 function setupCanvasSize() {
-  margin = {top:600 , left: 80, bottom: 20, right: 30};
-  width = 600 - margin.left - margin.right;
-  height = 300 - margin.left - margin.right;
+  margin = {top: 0, left: 80, bottom: 20, right: 30};
+  width = 960 - margin.left - margin.right;
+  height = 120 - margin.top - margin.bottom;
 }
 
 function appendSvg(domElement) {
@@ -69,15 +68,13 @@ function appendXAxis() {
   // Add the X Axis
   svg.append("g")
     .attr("transform",`translate(0, ${height})`)
-    .call(d3.axisTop(x))
-    .attr("transform", "rotate(270)"); 
+    .call(d3.axisBottom(x));
 }
 
 function appendYAxis() {
   // Add the Y Axis
   svg.append("g")
-  .call(d3.axisLeft(y))
-  .attr("transform", "rotate(-90)");
+  .call(d3.axisLeft(y));
 }
 
 function appendChartBars()
@@ -99,38 +96,13 @@ function appendChartBars()
     //    width: Now that we have the mapping previously done (linear)
     //           we just pass the sales and use the X axis conversion to
     //           get the right value
-
-    //helper that returns a color based on an ID
     newRects.append('rect')
       .attr('x', x(0))
       .attr('y', function(d, i) {
         return y(d.product);
       })
-      .attr('height', function(d, i) {
-            return y.bandwidth() - 5;
-      })
+      .attr('height', y.bandwidth)
       .attr('width', function(d, i) {
         return x(d.sales);
-      })
-      .attr('fill', function(d) {
-        return barColor(d.product);
-      })
-      .attr("transform", "rotate(-90)");
-    
-}
-
-function AppendLegend() {
-  // building a legend is as simple as binding
-  // more elements to the same data. in this case,
-  // <text> tags
-  svg.append('g')
-    .attr("transform",`translate(200, -500)`)
-    .attr('class', 'legend')
-      .selectAll('text')
-      .data(totalSales)
-        .enter()
-          .append('text')
-            .text(function(d) { return '• ' + d.product; })
-            .attr('fill', function(d) { return barColor(d.product); })
-            .attr('y', function(d, i) { return 20 * (i + 1); })  
+      });
 }
